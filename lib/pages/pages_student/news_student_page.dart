8d76599dbc9db597f5a0d80d67d6cffb0aeca9ca
hungdk <kiemhung2601@gmail.com>
2022-02-19 19:12:17 +0700

    Fix UI news_student_page

--- lib/pages/pages_student/news_student_page.dart
+++ lib/pages/pages_student/news_student_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:socialwork/pages/pages_student/details_news_page.dart';
 
 import '../../constant.dart';
@@ -12,8 +13,19 @@ class NewsStudentPage extends StatefulWidget {
 }
 
 class _NewsStudentPageState extends State<NewsStudentPage> {
-  Future<void> _refresh(){
-    return Future.delayed(Duration(seconds: 2));
+  int activeIndex = 0;
+
+  final colors = const [
+    Colors.red,
+    Colors.green,
+    Colors.greenAccent,
+    Colors.amberAccent,
+    Colors.blue,
+    Colors.amber,
+  ];
+
+  Future<void> _refresh() {
+    return Future.delayed(const Duration(seconds: 2));
   }
 
   Widget _buildAppBarView() {
@@ -38,67 +50,114 @@ class _NewsStudentPageState extends State<NewsStudentPage> {
     );
   }
 
-  Widget _buildCarouseSlider(BuildContext context) {
+  Widget _buildIndicator() =>
+      AnimatedSmoothIndicator(
+          activeIndex: activeIndex,
+          count: 5,
+        effect: CustomizableEffect(
+          activeDotDecoration: DotDecoration(
+            width: 32,
+            height: 12,
+            color: Colors.indigo,
+            rotationAngle: 180,
+            verticalOffset: -10,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          dotDecoration: DotDecoration(
+            width: 24,
+            height: 12,
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(16),
+            verticalOffset: 0,
+          ),
+          spacing: 6.0,
+          inActiveColorOverride: (i) => colors[i],
+        ),
+      );
+
+
+
+  Widget _buildScrollHorizontal(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return CarouselSlider(
-      options: CarouselOptions(height: 250.0),
-      items: [1, 2, 3, 4, 5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return InkWell(
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5),
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(
-                        'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg',
-                        fit: BoxFit.cover,
-                        height: 250,
-                      ),
-                    ),
-                    Container(
-                      width: size.width,
-                      height: 250,
-                      margin: const EdgeInsets.symmetric(horizontal: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        gradient: const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Color(0xCC000000),
-                              Color(0x00000000),
-                              Color(0x00000000),
-                              Color(0xCC000000),
-                            ]),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        margin: const EdgeInsets.all(20),
-                        child: const Text(
-                          'Đây chỉ là một đoạn title để test mà thôi nhé',
-                          style: TextStyle(fontSize: 15, color: Colors.white),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 5,),
+        CarouselSlider(
+          options: CarouselOptions(
+            height: size.height/3,
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            onPageChanged: (index, reason) =>
+                setState(() => activeIndex = index),
+            // pageSnapping: false,
+            // autoPlay: true,
+            // autoPlayInterval: Duration(seconds: 1),
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return InkWell(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg',
+                            fit: BoxFit.cover,
+                            height: size.height/3,
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            const DetailsNewsPage()));
+                        Container(
+                          width: size.width,
+                          height: size.height/3,
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: const LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Color(0xCC000000),
+                                  Color(0x00000000),
+                                  Color(0x00000000),
+                                  Color(0xCC000000),
+                                ]),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            margin: const EdgeInsets.all(20),
+                            child: const Text(
+                              'Đây chỉ là một đoạn title để test mà thôi nhé',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const DetailsNewsPage()));
+                  },
+                );
               },
             );
-          },
-        );
-      }).toList(),
+          }).toList(),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        _buildIndicator(),
+      ],
     );
   }
 
@@ -140,7 +199,8 @@ class _NewsStudentPageState extends State<NewsStudentPage> {
               ),
               Text(
                 getTruncatedTitle(title, 60),
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
               ),
               const SizedBox(
                 height: 20,
@@ -172,91 +232,20 @@ class _NewsStudentPageState extends State<NewsStudentPage> {
         'Đây chỉ là một đoạn test nho nhỏ thôi nhé, các bạn đọc kĩ nha. Cảm ơn mọi người';
     double _score = 0.5;
     String _date = 'Today, 8:28AM';
-
-    return Column(
-      children: [
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                        const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                        const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                        const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                    const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                    const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                    const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                    const DetailsNewsPage()));
-          },
-        ),
-        InkWell(
-          child: _viewEvent(_urlImage, _title, _score, _date),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                    const DetailsNewsPage()));
-          },
-        ),
-      ],
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return InkWell(
+              child: _viewEvent(_urlImage, _title, _score, _date),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                      const DetailsNewsPage()));
+            },
+          );
+        });
   }
 
   Widget _buildNewsPage() {
@@ -264,45 +253,22 @@ class _NewsStudentPageState extends State<NewsStudentPage> {
       triggerMode: RefreshIndicatorTriggerMode.onEdge,
       edgeOffset: 40,
       onRefresh: _refresh,
-      child: SingleChildScrollView(
-        reverse: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text(
-                'Xin chào',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text(
-                'Bắt đầu ngày mới bằng một việc năng động nào',
-                style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            _buildCarouseSlider(context),
-            const SizedBox(
-              height: 15,
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(flex: 5, child: _buildScrollHorizontal(context)),
+          Container(
+              margin: const EdgeInsets.all(10),
+              child: const Text(
                 'Danh sách sự kiện',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            _buildListEvent(),
-          ],
-        ),
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              )),
+          Expanded(flex: 6, child: _buildListEvent()),
+        ],
       ),
     );
   }
